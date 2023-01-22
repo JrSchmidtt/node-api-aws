8cc74252cff86e1a0f5c11d1441fa2d46a4500f6
JrSchmidtt <junior.schmidt.99@gmail.com>
2023-01-22 16:22:43 -0300

    infra beanstalk env

--- aws/terraform/beanstalk.tf
+++ aws/terraform/beanstalk.tf
@@ -5,3 +5,33 @@ resource "aws_elastic_beanstalk_application" "beanstalk_application" {
   name        = var.name
   description = var.description
 }
+
+resource "aws_elastic_beanstalk_environment" "beanstalk_environment" {
+  name = var.environment
+  application = aws_elastic_beanstalk_application.beanstalk_application.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.5.3 running Docker"              # Docker platform
+
+  # EC2 instance type
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "instanceType"
+    value     = var.machine_type
+  }
+
+  # Auto scaling group max size (number of instances)
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     =  var.max_instances
+  }
+
+  # IAM instance profile
+  # See https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/iam-instanceprofile.html
+  setting {
+    namespace = "aws:autoscaling:lauchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.beanstalk_ec2_profile.name
+  }
+}
+
+# Docs ElastiBeanstalk: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elastic_beanstalk_environment
--- aws/terraform/var.tf
+++ aws/terraform/var.tf
@@ -2,13 +2,36 @@
 # Terraform vars are defined in the terraform/var.tf file
 
 variable "name" {
-  type        = string                      # Type of the variable
-  description = "Name of the application"   # Description of the variable
-  default     = "my-app"                    # Default value of the variable
+  type        = string                    # Type of the variable
+  description = "Name of the application" # Description of the variable
+  default     = "my-app"                  # Default value of the variable
 }
 
 variable "description" {
-  type        = string                              # Type of the variable
-  description = "Description of the application"    # Description of the variable 
-  default     = "This is an example application"    # Default value of the variable
+  type        = string                           # Type of the variable
+  description = "Description of the application" # Description of the variable 
+  default     = "This is an example application" # Default value of the variable
+}
+
+# The environment is a string, but it must be a valid environment name
+# See https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/using-features.environments.html
+variable "environment" {
+  type        = string              # Type of the variable
+  description = "Environment name"  # Description of the variable
+  default     = "dev"               # Default value of the variable
+}
+
+# The machine type is a string, but it must be a valid EC2 instance type
+# See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-types.html
+variable "machine_type" {
+  type        = string              # Type of the variable
+  description = "Machine type"      # Description of the variable
+  default     = "t2.micro"          # Default value of the variable
+}
+
+# The max instances is a number, but it must be a valid number of instances
+variable "max_instances" {
+  type        = number              # Type of the variable
+  description = "Max instances"     # Description of the variable
+  default     = 1                   # Default value of the variable
 }
